1e38e8333110ae78e9320d4b1df96c3320fdee1c
Leon Mendonca <leonmendonca619@gmail.com>
2025-03-05 04:57:48 -0800

    controllers created

--- src/books_v2/books_v2.controller.ts
+++ src/books_v2/books_v2.controller.ts
@@ -1,66 +1,48 @@
 import { Controller, Get, Post, Body, Param, Put, Delete, Query, UsePipes, HttpException, HttpStatus, ParseUUIDPipe } from '@nestjs/common';
-import { BooksService } from 'src/books/books.service';
+import { BooksV2Service } from './books_v2.service';
 import { CreateBookDTO } from './dto/create-book.dto';
 import { UpdateBookDTO } from './dto/update-book.dto';
 import { Book } from './entities/book.entity';
 import { CreateBookValidationPipe } from './pipes/create-book-validation.pipe';
 import { UpdateBookValidationPipe } from './pipes/update-book-validation.pipe';
+import { QueryValidationPipe } from 'src/pipes/query-validation.pipe';
+import { bookQuerySchema } from 'src/books/zod-validation/bookquery-zod';
+import { BookQueryValidator, UnionBook } from 'src/books/books.query-validator';
+import { bodyValidationPipe } from 'src/pipes/body-validation.pipe';
+import { createBookSchema, TCreateBookDTO } from 'src/books/zod-validation/createbooks-zod';
 
 @Controller('books-v2')
 export class BooksV2Controller {
-  constructor(private readonly booksService: BooksService,) {}
+  constructor(private readonly booksService: BooksV2Service,) {}
 
   // Get all books
-  @Get()
-  async findAll(): Promise<Book[]> {
-    return await this.booksService.findAll();
+  @Get('all')
+  async getAllBooks() {
+    return this.booksService.getBooks();
   }
 
-  // Get book by ID
-  @Get(':id')
-  async findOne(
-    @Param('id', ParseUUIDPipe) id: string,
-  ): Promise<Book> {
-    const book = await this.booksService.findOne(id);
-    if (!book) {
-      throw new HttpException('Book not found', HttpStatus.NOT_FOUND);
+    @Get('search')
+  @UsePipes(new QueryValidationPipe(bookQuerySchema, BookQueryValidator))
+  async getBookBy(@Query() query: UnionBook) {
+    const result = await this.booksService.findBookBy(query);
+    if (result.length != 0) {
+      return result[0];
+    } else {
+      throw new HttpException('No book found', HttpStatus.NOT_FOUND);
     }
-    return book;
   }
 
   // Create new book
-  @Post()
-  @UsePipes(CreateBookValidationPipe)
-  async create(@Body() createBookDTO: CreateBookDTO): Promise<Book> {
+  @Post('create')
+  @UsePipes(new bodyValidationPipe(createBookSchema))
+  async createBook(@Body() bookPayload: TCreateBookDTO) {
     try {
-      return await this.booksService.create(createBookDTO);
+      return await this.booksService.createBook(bookPayload);
     } catch (error) {
-      throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
-    }
-  }
-
-  // Update book by ID
-  @Put(':id')
-  @UsePipes(UpdateBookValidationPipe)
-  async update(
-    @Param('id', ParseUUIDPipe) id: string,
-    @Body() updateBookDTO: UpdateBookDTO,
-  ): Promise<Book> {
-    try {
-      const updatedBook = await this.booksService.update(id, updateBookDTO);
-      if (!updatedBook) {
-        throw new HttpException('Book not found', HttpStatus.NOT_FOUND);
+      if (error instanceof Error) {
+        throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
       }
-      return updatedBook;
-    } catch (error) {
-      throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
     }
   }
 
-  // Delete book by ID
-  @Delete(':id')
-  async remove(
-    @Param('id', ParseUUIDPipe) id: string,
-  ): Promise<{ message: string }> {
-    try {
-      const deletedBook = await this
+} 
