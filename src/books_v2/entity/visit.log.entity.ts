032e7eb9c316b8419aafecfc24a0e5e62778dd7d
siddhesh_patwa <127028705+siddheshpatwa@users.noreply.github.com>
2025-03-12 17:13:57 +0530

    abc

--- src/books_v2/entity/visit.log.entity.ts
+++ src/books_v2/entity/visit.log.entity.ts
@@ -6,10 +6,10 @@ export class VisitLog {
   @PrimaryGeneratedColumn('uuid', { name: 'visitlog_id' })
   visitlogId:'visitlogid' = 'visitlogid';
 
-@Column({name:'student_id', type:'uuid'})
+@Column({name:'student_id', type:'uuid', nullable:true})
 StudentId :'student_id'='student_id';
 
-  @Column({ name: 'timestamp' })
+  @Column({ name: 'timestamp' , nullable:true})
   timestamp: 'timestamp'='timestamp';
 
   @Column({ name: 'action', type: 'varchar', length: 255 })
